5a2dd0e532c95d7744b8e6b5219333c445a52118
michaelstafford <Michael.Stafford@nopsema.gov.au>
2017-10-27 12:01:20 +1100

    [1466] make it work with JSONAPI

--- api/app/controllers/messages_controller.rb
+++ api/app/controllers/messages_controller.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class MessagesController < ApplicationController
+  before_action :set_message, only: %i[show update destroy]
+
   def index
-    json_response(message: "Hello World")
+    @messages = Message.all
+    json_response(@messages)
+  end
+
+  private
+  def messages_params
+    params.permit(:id, :text)
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :messages
+  scope "/api" do
+    scope "v1" do
+      resources :messages
+    end
+  end
 
   # Must come last
   mount_ember_app :ui, to: "/"
