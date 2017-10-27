913aad783276588318048600513e2ed2ebfceae0
michaelstafford <Michael.Stafford@nopsema.gov.au>
2017-10-27 12:32:06 +1100

    [#1466] add failing ember test and passing rails test

--- api/spec/controllers/messages_controller_spec.rb
+++ api/spec/controllers/messages_controller_spec.rb
@@ -6,7 +6,7 @@ require "rails_helper"
 RSpec.describe MessagesController, type: :request do
 
   context "Get /messages" do
-    before { get "/messages" }
+    before { get "/api/v1/messages" }
 
     it "returns messages" do
       expect(json).not_to be_empty
---
+++ ui/tests/unit/models/message.js
@@ -0,0 +1,8 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('model:message', 'Unit | message', {
+    unit: true
+});
+
+test('should dance like there is no tommorrow', function(assert) {
+});
