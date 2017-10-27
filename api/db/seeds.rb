b9e3ab5115b5082e884d02906d1927bdc01dfc1b
michaelstafford <Michael.Stafford@nopsema.gov.au>
2017-10-27 12:17:24 +1100

    [#1466] add fake data

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -6,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Message.all.size == 0 && ENV['RAILS_ENV'] != 'production'
+  5.times do
+    Message.create!(text: Faker::Hacker.ingverb)
+  end
+end
